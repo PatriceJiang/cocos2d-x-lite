db9e56d4d66229038ef52743e90ff283402a0a05
Arnold <397136899@qq.com>
2020-04-02 14:14:13 +0800

    use @

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -22,7 +22,7 @@ set -x
 
 echo "Build Android ... "
 cd $COCOS2DX_ROOT/templates/js-template-link/frameworks/runtime-src/proj.android-studio
-sed -i s|\$\{COCOS_X_ROOT\}|$COCOS2DX_ROOT|g app/build.gradle
+sed -i "s@\${COCOS_X_ROOT}@$COCOS2DX_ROOT@g" app/build.gradle
 echo "PROP_USE_CMAKE = true" >> gradle.properties 
 ./gradlew assembleDebug
 
